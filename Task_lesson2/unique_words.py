20ffa1df46f033d379a6dd78af232e4fa8ce9f44
Alex13101 <podiyachiy-a@mail.ru>
2023-04-28 15:03:41 +0300

    Разобраться с запятыми и ковычками в 3-м утверждении

---
+++ Task_lesson2/unique_words.py
@@ -0,0 +1,28 @@
+'''Контейнерные типы данных:
+Задача: Написать программу, которая принимает на вход строку и
+возвращает список всех уникальных слов в этой строке.
+При этом использовать только базовые операции, цикл for, функции и контейнерные типы данных.
+
+Тесты:'''
+
+
+
+
+def unique_words(lst):
+    finish_list = []
+    new_list = []
+    for i in lst.split():
+        if i in new_list:
+            continue
+        else:
+            new_list.append(i)
+    # for x in range(0, len(new_list)):
+    #     if "," in new_list[x]:
+    #         new_list.pop(-1)
+
+    print(new_list)
+    return new_list
+
+assert unique_words("hello world") == ["hello", "world"]
+assert unique_words("apple apple banana cherry") == ["apple", "banana", "cherry"]
+assert unique_words("Python is great, isn't it?") == ["Python", "is", "great,", "isn't", "it"]
