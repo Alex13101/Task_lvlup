594da9e8260882581b79629eea71767234f680ac
Evgenii Sergunin <mckrei@ya.ru>
2023-04-29 12:51:02 +0300

    dz

--- Task_lesson2/Calculate.py
+++ Task_lesson2/Calculate.py
@@ -7,50 +7,116 @@ assert calculate("1+2+3+4+5") == 15
 assert calculate("10*5-7*3+2") == 23'''
 
 
-
-
 def calculate(exp):
-    i = 0
+    # Определяем приоритеты операций
+    precedence = {"+": 1, "-": 1, "*": 2, "/": 2}
+    # Создаем стек для хранения операндов и операций
     stack = []
+    # Создаем список для хранения обратной польской записи
+    rpn = []
+    i = 0
     while i < len(exp):
         if exp[i].isdigit():
+            # Если символ является цифрой, то читаем число
             num = int(exp[i])
             i += 1
             while i < len(exp) and exp[i].isdigit():
                 num = num * 10 + int(exp[i])
                 i += 1
-            stack.append(num)
-        elif exp[i] == "+":
-            stack.append("+")
+            # Добавляем число в обратную польскую запись
+            rpn.append(num)
+        elif exp[i] in ["+", "-", "*", "/"]:
+            # Если символ является операцией, то помещаем ее в стек
+            while stack and stack[-1] != "(" and precedence[exp[i]] <= precedence.get(stack[-1], 0):
+                rpn.append(stack.pop())
+            stack.append(exp[i])
             i += 1
-        elif exp[i] == "-":
-            stack.append("-")
+        elif exp[i] == "(":
+            # Если символ является открывающей скобкой, то помещаем ее в стек
+            stack.append("(")
             i += 1
-        elif exp[i] == ("*"):
-            stack.append("*")
-            i += 1
-        elif exp[i] == ("/"):
-            stack.append("/")
+        elif exp[i] == ")":
+            # Если символ является закрывающей скобкой, то извлекаем операции из стека
+            while stack and stack[-1] != "(":
+                rpn.append(stack.pop())
+            if stack and stack[-1] == "(":
+                stack.pop()
             i += 1
         else:
+            # Пропускаем пробелы и другие символы
             i += 1
-    print(stack)
-    result = stack[0]
-    for i in range(1, len(stack), 2):
-        if stack[i] == "+":
-            result += stack[i + 1]
-        elif stack[i] == "*":
-            result *= stack[i + 1]
-        elif stack[i] == "/":
-            result /= stack[i + 1]
+    # Извлекаем оставшиеся операции из стека
+    while stack:
+        rpn.append(stack.pop())
+    # Вычисляем результат, используя обратную польскую запись
+    stack = []
+    for token in rpn:
+        if isinstance(token, int):
+            # Если символ является числом, то помещаем его в стек
+            stack.append(token)
         else:
-            result -= stack[i + 1]
-
+            # Если символ является операцией, то извлекаем операнды из стека
+            b = stack.pop()
+            a = stack.pop()
+            # Выполняем операцию и помещаем результат в стек
+            if token == "+":
+                stack.append(a + b)
+            elif token == "-":
+                stack.append(a - b)
+            elif token == "*":
+                stack.append(a * b)
+            elif token == "/":
+                stack.append(a / b)
+    # Результат вычисления находится на вершине стека
+    result = stack[-1]
     print("The evaluated result is:", result)
     return result
 
-#stack = []
-assert calculate("2+3*4-5/2") == 7.5
-assert calculate("1+2+3+4+5") == 15
-assert calculate("10*5-7*3+2") == 23
+# def calculate(exp):
+#     i = 0
+#     stack = []
+#     while i < len(exp):
+#         if exp[i].isdigit():
+#             num = int(exp[i])
+#             i += 1
+#             while i < len(exp) and exp[i].isdigit():
+#                 num = num * 10 + int(exp[i])
+#                 i += 1
+#             stack.append(num)
+#         elif exp[i] == "+":
+#             stack.append("+")
+#             i += 1
+#         elif exp[i] == "-":
+#             stack.append("-")
+#             i += 1
+#         elif exp[i] == ("*"):
+#             stack.append("*")
+#             i += 1
+#         elif exp[i] == ("/"):
+#             stack.append("/")
+#             i += 1
+#         else:
+#             i += 1
+#     print(stack)
+#     result = stack[0]
+#     for i in range(1, len(stack), 2):
+#         if stack[i] == "+":
+#             result += stack[i + 1]
+#         elif stack[i] == "*":
+#             result *= stack[i + 1]
+#         elif stack[i] == "/":
+#             result /= stack[i + 1]
+#         else:
+#             result -= stack[i + 1]
 
+#     print("The evaluated result is:", result)
+#     return result
+
+#stack = []
+# assert calculate("2+3*4-5/2") == 7.5
+# assert calculate("1+2+3+4+5") == 15
+# assert calculate("10*5-7*3+2") == 23
+
+assert calculate("2+3*4-5/2") == 11.5
+assert calculate("1+2+3+4+5") == 15
+assert calculate("10*5-7*3+2") == 31
--- Task_lesson2/unique_words.py
+++ Task_lesson2/unique_words.py
@@ -8,21 +8,46 @@
 
 
 
-def unique_words(lst):
-    finish_list = []
-    new_list = []
-    for i in lst.split():
-        if i in new_list:
-            continue
-        else:
-            new_list.append(i)
-    # for x in range(0, len(new_list)):
-    #     if "," in new_list[x]:
-    #         new_list.pop(-1)
+# def unique_words(lst):
+#     finish_list = []
+#     new_list = []
+#     for i in lst.split():
+#         if i in new_list:
+#             continue
+#         else:
+#             new_list.append(i)
+#     # for x in range(0, len(new_list)):
+#     #     if "," in new_list[x]:
+#     #         new_list.pop(-1)
 
-    print(new_list)
-    return new_list
+    # print(new_list)
+    # return new_list
+
+
+def unique_words(string):
+    # создаем пустой список для уникальных слов
+    unique_words = []
+
+    stop_symbols = r'.,!?;:()[]{}<>'
+
+    # удаляем из строки все знаки препинания
+    for stop_symbol in stop_symbols:
+        string = string.replace(stop_symbol, '')
+
+    # разбиваем строку на список слов
+    words = string.split()
+
+    # проходим циклом по каждому слову и добавляем его в список
+    # уникальных слов, если оно еще не было добавлено
+    for word in words:
+        if word not in unique_words:
+            unique_words.append(word)
+
+    return unique_words
+
+
+
 
 assert unique_words("hello world") == ["hello", "world"]
 assert unique_words("apple apple banana cherry") == ["apple", "banana", "cherry"]
-assert unique_words("Python is great, isn't it?") == ["Python", "is", "great,", "isn't", "it?"]
+assert unique_words("Python is great, isn't it?") == ["Python", "is", "great", "isn't", "it"]
